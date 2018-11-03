cde37a1c804b234df0ece346993f97b0452c2861
Marika Ross <marika.ross@gmail.com>
2018-11-03 14:39:50 -0600

    Add react animated weather icons to display

--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import ReactAnimatedWeather from 'react-animated-weather';
 
 
 const Display = ({conditions}) => {
     console.log(conditions)
+
   return(
-    <h1>Potato</h1>
+    <div className="display"> 
+       <ReactAnimatedWeather
+        icon={conditions.icon}
+        color={'black'}
+        size={100}
+        animate={true}
+      />
+      <h3>{conditions.temperature}</h3>
+      <h4>{conditions.summary}</h4>
+    </div>
+
     )
 }
 
